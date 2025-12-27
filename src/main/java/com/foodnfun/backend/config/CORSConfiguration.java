a6eb55e6a992ec32f1f07629c5a5dd00485755fa
Nitin Ranajn <156094474+Nit02@users.noreply.github.com>
2025-12-27 22:38:23 +0530

    Update CORSConfiguration.java

--- src/main/java/com/foodnfun/backend/config/CORSConfiguration.java
+++ src/main/java/com/foodnfun/backend/config/CORSConfiguration.java
@@ -15,7 +15,7 @@ public class CORSConfiguration {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")  // all endpoints
-	                        .allowedOrigins("*") // allow all origins for now
+	                        .allowedOriginsPatterns("*") // allow all origins for now
 	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                        .allowedHeaders("*")
 	                        .allowCredentials(true);
