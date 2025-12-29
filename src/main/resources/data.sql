0de772a48591a132938a9275e62c492e93fdc5a8
Nitin Ranajn <156094474+Nit02@users.noreply.github.com>
2025-12-29 11:53:12 +0530

    Add dummy food data and enable sql initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO foods (name,description,price,image_url) VALUES
+('Margherita Pizza', 'Classic cheese pizza with tomato base', 199.00, 'https://example.com/images/pizza.jpg'),
+('Veg Burger', 'Crispy veggie patty with fresh lettuce', 99.00, 'https://example.com/images/burger.jpg'),
+('Chicken Biryani', 'Aromatic basmati rice with spices', 249.00, 'https://example.com/images/biryani.jpg'),
+('White Sauce Pasta', 'Creamy pasta with Italian herbs', 179.00, 'https://example.com/images/pasta.jpg'),
+('Chocolate Ice Cream', 'Rich chocolate scoop dessert', 79.00, 'https://example.com/images/icecream.jpg');
